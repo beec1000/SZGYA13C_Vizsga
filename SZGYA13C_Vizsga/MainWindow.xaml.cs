4cf97f14636f083f55c0b37c4d565a52ec00882c
beec1000 <beec1000.minecraft@gmail.com>
2025-01-17 12:22:36 +0100

    Add project files.

---
+++ SZGYA13C_Vizsga/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using SZGYA13C_Vizsga;
+
+namespace SZGYA13C_Vizsga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Vizsgazo> vizsgazok = new List<Vizsgazo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            sikeresLB.Content = string.Empty;
+
+            vizsgazok = Vizsgazo.Fromfile(@"..\..\..\src\adatok.txt");
+
+            var nevek = vizsgazok.Select(n => n.Nev);
+            foreach (var n in nevek)
+            {       
+                vizsgazokList.Items.Add(n);
+            }
+
+            vizsgazokLB.Content = $"{vizsgazok.Count} vizsgázó adatait beolvastuk";
+
+            
+        }
+
+        private void sikeresBTN_Click(object sender, RoutedEventArgs e)
+        {
+            sikeresLB.Content = string.Empty;
+            var eredmeny = vizsgazok.Count(e => e.Erdemjegy() != "Elégtelen");
+            sikeresLB.Content = $"{eredmeny} fő";
+        }
+
+        private void allomanybaIrasBTN_Click(object sender, RoutedEventArgs e)
+        {
+            var sikeres = vizsgazok.Select(s => $"{s.Nev}\t{Math.Round(s.Vegeredmeny, 2)}\t{s.Erdemjegy()}").ToList();
+
+            File.WriteAllLines(@"..\..\..\src\vizsgaredmenyek.txt", sikeres);
+
+            if (sikeres != null)
+            {
+                MessageBox.Show("Sikeres!", "Siker", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Hiba!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ SZGYA13C_Vizsga/Vizsgazo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SZGYA13C_Vizsga
+{
+    internal class Vizsgazo
+    {
+        public string Nev { get; set; }
+        public float ITesHalozati { get; set; }
+        public float Programozas { get; set; }
+        public float HalozatAModul { get; set; }
+        public float HalozatBModul { get; set; }
+        public float HalozatCModul { get; set; }
+        public float HalozatDModul { get; set; }
+        public float ITSzobeli { get; set; }
+        public float AngolSzobeli { get; set; }
+        private IEnumerable<float> ModulEredmenyek => new[]
+        {
+        ITesHalozati, Programozas, HalozatAModul, HalozatBModul, 
+        HalozatCModul, HalozatDModul, ITSzobeli, AngolSzobeli
+        };  
+
+        public float Vegeredmeny => ModulEredmenyek.Average();
+
+        public Vizsgazo(string nev, float ith, float programozas, float halozata, float halozatb, float halozatc, float halozatd, float itsz, float angolsz)
+        {
+            Nev = nev;
+            ITesHalozati = ith;
+            Programozas = programozas;
+            HalozatAModul = halozata;
+            HalozatBModul = halozatb;
+            HalozatCModul = halozatc;
+            HalozatDModul = halozatd;
+            ITSzobeli = itsz;
+            AngolSzobeli = angolsz;
+        }
+
+        public string Erdemjegy()
+        {
+            if(ModulEredmenyek.Any(e => e < 0.51f))
+            {
+                return "Elégtelen";
+            }
+            else
+            {
+                return Vegeredmeny switch
+                {
+                    >= 0.81f => "Jeles",
+                    >= 0.71f => "Jó",
+                    >= 0.61f => "Közepes",
+                    >= 0.51f => "Elégséges",
+                };
+            }
+        }
+
+        public static List<Vizsgazo> Fromfile(string p)
+        {
+            List<Vizsgazo> vizsgazo = new List<Vizsgazo>();
+
+            string[] lines = File.ReadAllLines(p);
+
+            foreach (var l in lines)
+            {
+                string[] v = l.Split(';');
+
+                string Nev = v[0];
+                float ITesHalozati = float.Parse(v[1]);
+                float Programozas = float.Parse(v[2]);
+                float HalozatAModul = float.Parse(v[3]);
+                float HalozatBModul = float.Parse(v[4]);
+                float HalozatCModul = float.Parse(v[5]);
+                float HalozatDModul = float.Parse(v[6]);
+                float ITSzobeli = float.Parse(v[7]);
+                float AngolSzobeli = float.Parse(v[8]);
+
+                Vizsgazo vizsgazok = new(Nev, ITesHalozati, Programozas, HalozatAModul, HalozatBModul, HalozatCModul, HalozatDModul, ITSzobeli, AngolSzobeli);
+                vizsgazo.Add(vizsgazok);
+            }
+
+            return vizsgazo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev}, {ITesHalozati}, {Programozas}, {HalozatAModul}, {HalozatBModul}, {HalozatCModul}, {HalozatDModul}, {ITSzobeli}, {AngolSzobeli}";
+        }
+
+    }
+}
